35981266447e0c67844668fe5f23697997011cba
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-21 16:32:57 +0900

    인트로뷰 수정2
    인트로뷰 수정2

--- app/src/main/java/com/example/elab_yang/treadmill/activity/IntroActivity.java
+++ app/src/main/java/com/example/elab_yang/treadmill/activity/IntroActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.elab_yang.treadmill.R;
@@ -27,6 +28,8 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        // 상태바 제거
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Button next_btn = (Button) findViewById(R.id.next_btn);
         next_btn.setOnClickListener(v -> {
             // 시작하기
